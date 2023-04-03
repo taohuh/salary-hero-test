2830957681c09ae4a161376f04de5361dc8800e3
taohuh <pimpakarn.wan@gmail.com>
2023-04-03 13:42:36 +0700

    test: add browser cookies file

---
+++ src/mock/cookies.ts
@@ -0,0 +1,20 @@
+export const browserCookies = [
+  {
+    name: 'language',
+    value: 'en',
+    domain: 'salaryhero-qa-challenge.herokuapp.com',
+    path: '/',
+  },
+  {
+    name: 'cookieconsent_status',
+    value: 'dismiss',
+    domain: 'salaryhero-qa-challenge.herokuapp.com',
+    path: '/',
+  },
+  {
+    name: 'welcomebanner_status',
+    value: 'dismiss',
+    domain: 'salaryhero-qa-challenge.herokuapp.com',
+    path: '/',
+  },
+]
